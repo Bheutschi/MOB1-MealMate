603a66cf3b9bc277a3c1275f033f7afd853dd64a
bheutschi <bheutschi@gmail.com>
2026-05-18 10:31:00 +0200

    feat: add Meal model with properties and constructor

---
+++ lib/models/meal.dart
@@ -0,0 +1,25 @@
+class Meal {
+  final String id;
+  final String name;
+  final String category;
+  final String area;
+  final String country;
+  final String instructions;
+  final String imageUrl;
+  final String? youtubeUrl;
+  final List<String> ingredients;
+  final List<String> measures;
+
+  Meal({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.area,
+    required this.country,
+    required this.instructions,
+    required this.imageUrl,
+    this.youtubeUrl,
+    this.ingredients = const [],
+    this.measures = const [],
+  });
+}
