c10ee0ea29a4e26a01fad013637a5612e7400c8b
bheutschi <bheutschi@gmail.com>
2026-06-01 11:04:56 +0200

    feat: add theme persistence and toggle logic to ThemeProvider

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/foundation.dart';
+import '../services/storage_service.dart';
 
 class ThemeProvider extends ChangeNotifier {
+  final StorageService _storage = StorageService();
 
+  bool _isDarkMode = false;
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeProvider() {
+    _loadFromStorage();
+  }
+
+  Future<void> toggle() async {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+    await _storage.saveDarkMode(_isDarkMode);
+  }
+
+  Future<void> _loadFromStorage() async {
+    try {
+      _isDarkMode = await _storage.loadDarkMode();
+      notifyListeners();
+    } catch (e) {
+      debugPrint('Error loading theme preference: $e');
+    }
+  }
 }
